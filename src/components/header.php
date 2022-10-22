f36b4d4933adf65579e9669964056f69f518a13b
quoccuongjk <ngoquoccuongfml1@gmail.com>
2022-10-22 08:03:20 +0700

    update header

--- src/components/header.php
+++ src/components/header.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style_cuong.css">
     
-    <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../styles/vendors/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
   <div class="container-fluid">
