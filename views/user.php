e4bf3ecf91b3ae5e04d6d6b60ba5f902bf7ea706
quoccuongjk <ngoquoccuongfml1@gmail.com>
2022-11-08 20:19:57 +0700

    quan ly tai khoang xong

--- views/user.php
+++ views/user.php
@@ -69,7 +69,10 @@
             $user_phone = mysqli_real_escape_string($this->db->link,$user_phone);
             $user_name = mysqli_real_escape_string($this->db->link,$user_name);
             $user_address = mysqli_real_escape_string($this->db->link,$user_address);
-            
+            if(empty($user_email) || empty($user_password) || empty($user_phone)|| empty($user_name)|| empty($user_address) ) {
+                $alert = "<span class = 'error'>Not empty</span>";
+                return $alert;
+            }
             $check_email = "SELECT * FROM tbl_user WHERE user_email='$user_email' LIMIT 1 ";
             $result_check = $this->db->select($check_email);
             if($result_check){
@@ -81,6 +84,34 @@
                  $result = $this->db->insert($query);
                  if($result){
                     header('Location:login.php');
+                 } else{
+                    $alert = "<span class = 'error'>User created not successfully</span>";
+                return $alert;
+                 }
+            }
+        }
+        public function insert_user_($user_email,$user_password,$user_phone,$user_name, $user_address){
+            $user_email = mysqli_real_escape_string($this->db->link,$user_email);
+            $user_password = mysqli_real_escape_string($this->db->link,$user_password);
+            $user_phone = mysqli_real_escape_string($this->db->link,$user_phone);
+            $user_name = mysqli_real_escape_string($this->db->link,$user_name);
+            $user_address = mysqli_real_escape_string($this->db->link,$user_address);
+            if(empty($user_email) || empty($user_password) || empty($user_phone)|| empty($user_name)|| empty($user_address) ) {
+                $alert = "<span class = 'error'>Not empty</span>";
+                return $alert;
+            }
+            $check_email = "SELECT * FROM tbl_user WHERE user_email='$user_email' LIMIT 1 ";
+            $result_check = $this->db->select($check_email);
+            if($result_check){
+                $alert = "<span class = 'error'>Email already existed</span>";
+                return $alert;
+            } else{
+                $query = "INSERT INTO tbl_user(user_email,user_password,user_phone,user_name,user_address)
+                 VALUES('$user_email','$user_password','$user_phone','$user_name','$user_address')";
+                 $result = $this->db->insert($query);
+                 if($result){
+                    $alert = "<span >User created successfully</span>";
+                return $alert;
                  } else{
                     $alert = "<span class = 'error'>User created not successfully</span>";
                 return $alert;
