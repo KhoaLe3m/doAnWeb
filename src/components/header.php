702ea6189a2dc22b30bb491367e99ffc2e46bb73
KhoaLe3m <lekhoa733@gmail.com>
2022-11-08 17:41:52 +0700

    feat: update

--- src/components/header.php
+++ src/components/header.php
@@ -18,7 +18,6 @@ UserController::init();
     <link rel="stylesheet" type="text/css" href="style_cuong.css">
 
     <link rel="stylesheet" href="../styles/vendors/font-awesome/css/font-awesome.min.css">
-<<<<<<< HEAD
   </head>
 
   <body>
@@ -37,16 +36,16 @@ UserController::init();
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                          <a class="nav-link" href="adidasview.php">Adidas <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="adidas_view.php">Adidas <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item active">
-                          <a class="nav-link" href="nike.php">Nike <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="nike_view.php">Nike <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item active">
-                          <a class="nav-link" href="jordan.php">Jordan <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="jordan_view.php">Jordan <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item active">
-                          <a class="nav-link" href="yeezy.php">Yeezy <span class="sr-only">(current)</span></a>
+                          <a class="nav-link" href="yeezy_view.php">Yeezy <span class="sr-only">(current)</span></a>
                         </li>
                       </ul>
                       <form class="form-inline my-2 my-lg-0" id="myform" action="search.php">
@@ -64,79 +63,19 @@ UserController::init();
                           UserController::destroy();
                         }
                         ?>
-                        <li class="nav-item active" style="padding-top: 5px;">
-                          <a class="nav-link" href="cart_invoke.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> <span class="sr-only">(current)</span></a>
-                        </li>
                         <li class="nav-item active ">
                           <a class="mr-2 nav-link" href="?action=logout"><i class="fa fa-sign-in" aria-hidden="true"></i> <?php echo UserController::checkSign() ?> <span class="sr-only">(current)</span></a>
+                        </li>
+
+
+                        <li class="nav-item active">
+                          <a class="nav-link" href="cart_invoke.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> <span class="sr-only">(current)</span></a>
                         </li>
                       </ul>
                     </div>
                   </nav>
                 </div>
-=======
-</head>
-<body>
-  <div class="container-fluid">
-    <div id="header">
-      <header>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="header bg-secondary">
-              <div class="menu w-100 h-100">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light" style="z-index:10">
-                  <a class="navbar-brand" href="index.php">SHOP</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                        <a class="nav-link" href="adidas_view.php">Adidas <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item active">
-                        <a class="nav-link" href="nike_view.php">Nike <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item active">
-                        <a class="nav-link" href="jordan_view.php">Jordan <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item active">
-                        <a class="nav-link" href="yeezy_view.php">Yeezy <span class="sr-only">(current)</span></a>
-                      </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0" id = "myform" action = "search.php">
-                      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id = textsearch name = "keyword";>
-                      <btn class="block btn"><i class="fa fa-search" aria-hidden="true" type="submit" onclick ="submitform()"></i></btn>
-                    </form>
-                     <script>
-                      function submitform(){
-                        document.getElementById('myform').submit();
-                      }
-                     </script>
-                    <ul class="ml-3 navbar-nav">
-                    <?php 
-                      if(isset($_GET['action'])&& $_GET['action']=='logout'){
-                        UserController::destroy();
-                      }
-                    ?>
-                      <li class="nav-item active ">
-                        <a class="mr-2 nav-link" href="?action=logout"><i class="fa fa-sign-in" aria-hidden="true"></i> <?php echo UserController::checkSign() ?> <span
-                            class="sr-only">(current)</span></a>
-                      </li>
-                    
-                     
-                      <li class="nav-item active">
-                        <a class="nav-link" href="cart_invoke.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> <span
-                            class="sr-only">(current)</span></a>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
->>>>>>> 35990c432d3b9918ad53ce46e2dc82927f7a5b4e
               </div>
             </div>
-          </div>
         </header>
       </div>
